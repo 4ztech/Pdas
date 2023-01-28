c7ef8d94dc76066b796da37ca11e709e8281f711
4ztech <mebckid@gmail.com>
2023-01-28 12:12:03 -0600

    Added images

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,9 +1,14 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/';
+
 
 function App(){
+    
+
     return(
         <div>
             <div> Personal Digital Assistants</div>
+            <img src="AlexaImage"/>
             <ProfileCard title="Alexa" handle="@alexa99"/>
             <ProfileCard title="Cortana" handle="@cortana32"/>
             <ProfileCard title="Siri" handle="@siri01"/>
