93cdedba458b7cb7e89673bafd788cdae9874b7a
4ztech <mebckid@gmail.com>
2023-01-28 12:19:44 -0600

    Corrected Images
    Didn't put Brackets, gotta check my notes

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,5 +1,7 @@
 import ProfileCard from "./ProfileCard";
-import AlexaImage from './images/';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png'
 
 
 function App(){
@@ -8,7 +10,11 @@ function App(){
     return(
         <div>
             <div> Personal Digital Assistants</div>
-            <img src="AlexaImage"/>
+            <img src={AlexaImage}/>
+            <img src={CortanaImage}/>
+            <img src={SiriImage}/>
+
+
             <ProfileCard title="Alexa" handle="@alexa99"/>
             <ProfileCard title="Cortana" handle="@cortana32"/>
             <ProfileCard title="Siri" handle="@siri01"/>
