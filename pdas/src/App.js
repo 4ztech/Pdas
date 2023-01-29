cb805ae884bed5678153935a2e77fdc83b93ab75
4ztech <mebckid@gmail.com>
2023-01-29 12:10:39 -0600

    Finished this App
    Quite interesting, learned a lot about props, css libs and how to use them.

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
@@ -9,15 +10,51 @@ function App(){
 
     return(
         <div>
-            <div> Personal Digital Assistants</div>
-            <img src={AlexaImage}/>
-            <img src={CortanaImage}/>
-            <img src={SiriImage}/>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>
+                        Personal Digital Assistants
+                    </p>
+                    <p className='subtitle'>
+                        PDA's
+                    </p>
+                </div>
+            </section>
 
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        {/* Here goes Alexa */}
+                        <div className='column is-2'>
+                            <ProfileCard 
+                            title="Alexa" 
+                            handle="@alexa99" 
+                            image={AlexaImage}
+                            description="Alexa was made by Amazon, Jeff Bezos BI is quite astonishing"
+                            />
+                        </div>
+                        {/* Here goes Cortana */}
+                        <div className='column is-2'>
+                            <ProfileCard 
+                            title="Cortana" 
+                            handle="@cortana32" 
+                            image={CortanaImage}
+                            description="Cortana was made by Microsoft, I love Seattle and love Bill Gates"
+                            />
+                        </div>
+                        {/* Here goes Siri */}
+                        <div className='column is-2'>
+                            <ProfileCard 
+                            title="Siri" 
+                            handle="@siri01" 
+                            image={SiriImage}
+                            description="Siri was made by Apple, Steve Jobs was my inspiration to get into SE"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
 
-            <ProfileCard title="Alexa" handle="@alexa99"/>
-            <ProfileCard title="Cortana" handle="@cortana32"/>
-            <ProfileCard title="Siri" handle="@siri01"/>
         </div>
     );
 }
